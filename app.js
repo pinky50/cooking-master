d0b7cc1cc7ced6be881d953e498654e4b3fa67ad
Your Name <you@example.com>
2021-02-07 22:48:37 +0600

    not found result styled

--- app.js
+++ app.js
@@ -1,61 +1,56 @@
 
-        document.getElementById("search").addEventListener("click",function(){
-            // console.log("btn clicked")
-           const foodItem =  document.getElementById("fooditem").value;
-           getFoods(foodItem)
-        //    console.log(foodItem);
-        //    const post = {strMeal:foodItem}
+document.getElementById("search").addEventListener("click", function () {
+    const foodItem = document.getElementById("fooditem").value;
+    getFoods(foodItem)
+    //    console.log(foodItem);
+})
 
-        })
+const getFoods = value => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+        .then(response => response.json())
+        .then(data => {
+            searchResult(data)
+        }).catch(err => {
+            resultNotFound("This food item is not available.")
+        });
+}
 
-        // strMeal
+const resultNotFound = value => {//for error show
+    const searchResult = document.getElementById('search-result')
+    searchResult.innerHTML = `<h2 class="not-found">${value}</h2>`
+}
 
-        const getFoods = value =>{
-            fetch( `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
-            .then(response => response.json())
-            .then(data => {
-                searchResult(data)
-            }).catch(err =>{
-                resultNotFout("Result not found")
-            });
-        }
-
-    const resultNotFout = value =>{
-        const searchResult = document.getElementById('search-result')
-        searchResult.innerHTML = `<h3>${value}</h3>`
-    }
-
-     const searchResult = result =>{
-         const searchResult = document.getElementById('search-result')
-         result.meals.forEach(element => {
-            searchResult.innerHTML += `
-                <div id="food-view" onClick="foodDetaile(${element.idMeal})" >
+const searchResult = result => {//search result
+    const searchResult = document.getElementById('search-result')
+    result.meals.forEach(element => {
+        searchResult.innerHTML += `
+                <div id="food-view" onClick="foodDetails(${element.idMeal})" >
                     <img class = "image" src="${element.strMealThumb}"  alt="" />
                     <h3>${element.strMeal}</h3>
                 </div>
             `
-         });         
-     }
+    });
+}
 
-     const foodDetaile = maleId =>{
-        console.log(maleId)
-        fetch( `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${maleId}`)
+const foodDetails = mailId => {
+    // console.log(mailId)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mailId}`)
         .then(response => response.json())
         .then(data => {
             veiwFoodDetails(data)
-        }).catch(err =>{
+        }).catch(err => {
             console.log(err)
         });
-     }
+}
 
-     const veiwFoodDetails = details =>{
-        const foodView = document.getElementById('food-view');
-        foodView.style.display = "none";
-        
-        const foodDetails = document.getElementById('food-details');
-        details.meals.forEach(element => {
-            foodDetails.innerHTML = `
-                <div onClick="foodDetaile(${element.idMeal})" >
+const veiwFoodDetails = details => {//for food item hide
+    const foodView = document.getElementById('food-view');
+    foodView.style.display = "none";
+
+    const foodDetails = document.getElementById('food-details');//After clicking on food item
+    details.meals.forEach(element => {
+        foodDetails.innerHTML = `
+                <div onClick="foodDetails(${element.idMeal})" >
                     <img class = "image" src="${element.strMealThumb}"  alt="" />
                     <h3>${element.strMeal}</h3>
                     <h5>Ingredients</h5>
@@ -66,11 +61,12 @@
                         <li>${element.strIngredient4}</li>
                         <li>${element.strIngredient5}</li>
                         <li>${element.strIngredient6}</li>
+                        <li>${element.strIngredient7}</li>
                     </ul>
                 </div>
             `
-         });   
-     }
+    });
+}
 
-      
-       
+
+
